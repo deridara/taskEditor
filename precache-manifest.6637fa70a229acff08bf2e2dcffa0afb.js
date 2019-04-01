000257b32f9db2646aff88ab9229acbb8d875b56
dara <teriaderide@gmail.com>
2019-04-01 16:59:24 +0300

    build

---
+++ precache-manifest.6637fa70a229acff08bf2e2dcffa0afb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "6d0aec9d4caa25db87bc",
+    "url": "./static/js/main.5513a6c4.chunk.js"
+  },
+  {
+    "revision": "47d04c44fbdb75d9789c",
+    "url": "./static/js/2.301028d0.chunk.js"
+  },
+  {
+    "revision": "6200f94aaf4aca924454f1fe30d4bc1d",
+    "url": "./index.html"
+  }
+];
